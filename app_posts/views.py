359e6867e7e5906608c577da69afe6d8c6b86515
Piotr Górski <piotrkropagorski@gmail.com>
2024-03-17 08:07:26 +0100

    Created Header

--- app_posts/views.py
+++ app_posts/views.py
@@ -4,3 +4,8 @@ def home_view(request):
     title = "Welcome to Django"
     
     return render(request, 'index.html', {'title': title})
+
+
+def index_view(request):
+    
+    return render(request,  'base.html')
--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from app_posts.views import home_view
+from app_posts.views import home_view, index_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
+    path('index/', index_view),
 ]
